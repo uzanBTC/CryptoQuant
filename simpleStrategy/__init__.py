ee2c65b9556020da25b2d335ab865d85ffea6b90
wzh6886668 <8774027+wzh6886668@user.noreply.gitee.com>
2022-12-06 15:12:28 +0800

    sma trial

---
+++ simpleStrategy/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022-12-06 14:00
+# @Author  : Wu Zhenhuan
+# @FileName: __init__.py.py
+# @Software: PyCharm
+
+ 
--- simpleStrategy/threeCloseStrategy.py
+++ simpleStrategy/threeCloseStrategy.py
@@ -106,7 +106,7 @@ if __name__ == '__main__':
     # Datas are in a subfolder of the samples. Need to find where the script is
     # because it could have been called from anywhere
     modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
-    datapath = os.path.join(modpath, 'TSLA.csv')
+    datapath = os.path.join(modpath, '../data/TSLA.csv')
 
     data = bt.feeds.YahooFinanceCSVData(
         dataname=datapath,
